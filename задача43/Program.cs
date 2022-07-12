6b3c142e34db11f9d6eac3e0a63ea7f030e3d133
Lena <imagine.youeself19@gmail.com>
2022-07-12 14:24:55 +0300

    добавлено решение задачи 43

---
+++ задача43/Program.cs
@@ -0,0 +1,15 @@
+﻿double b1, k1, b2, k2, x, y;
+Console.WriteLine("Введите число b1");
+b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число k1");
+k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число b2");
+b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число k2");
+k2 = Convert.ToDouble(Console.ReadLine());
+// Приравниваем правые части уравнений друг к другу чтобы получить  x: 
+// k1 * x + b1 = k2 * x + b2;
+x = (b2 - b1) / (k1 - k2);
+// Подставляем х в любое из уравнений, чтобы получить y:
+y = k1 * (b2 - b1) / (k1 - k2) + b1;
+Console.WriteLine($"Координаты точки пересечения двух прямых равна {(x, y)}");
